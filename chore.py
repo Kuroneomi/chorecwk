dd6e8d68ed3ea209590bfa23f2d71551eb4b5e61
Kuroneomi <47318281+Kuroneomi@users.noreply.github.com>
2019-02-04 23:50:23 +0000

    Update chore.py

--- chore.py
+++ chore.py
@@ -9,56 +9,58 @@
 #
 # Date: Feb 2019
 #
-
-def main():
-#define variables for menu
-    about = "a"
-    create_household = "c"
-    view_household = "v"
-    log_chores_done = "l"
-    show_leaderboard = "s"
-    quit = "q"
+def main() :
+#define variables
+  about = "a"
+  create_household = "c"
+  view_household = "v"
+  log_chores_done = "l"
+  show_leaderboard = "s"
+  quit = "q"
 #show user main menu
 #input for chore list
-    print("""Welcome to Chore Chart
-        \n\tAbout \t\t\t(A)
-        \tCreate Household \t(C)
-        \tView Household \t\t(V)
-        \tLog Chores Done \t(L)
-        \tShow Leaderboard \t(S)
-        \tQuit \t\t\t(Q)""")
-    while True:
+  print("""Welcome to Chore Chart
+    \n\tAbout \t\t\t(A)
+    \tCreate Household \t(C)
+    \tView Household \t\t(V)
+    \tLog Chores Done \t(L)
+    \tShow Leaderboard \t(S)
+    \tQuit \t\t\t(Q)""")
+  while True:
     chore_list = str(input("\n\tPlease choose an option and press <Enter>:"))
-#if about show explanationof program
+#if about execute
     if chore_list == about:
-        print("""This program helps chore management,
-        by logging the chores and who is tasked with a chore""")
-        continue
-#if create neomi to finish
+      print("This program helps chore management.")
+      continue
+#if create execute
     elif chore_list == create_household:
-        print("CREATING TO COMPLETE.")
-        continue
-#if view neomi to finish
+      print("CREATING TO COMPLETE.")
+      continue
+#if view execute
     elif chore_list == view_household:
-        print("VIEW TO COMPLETE.")
-        continue
-#if log done tell user incomplete
+      household_name = str(input("\nEnter the household name: "))
+      if household_name == household:
+        print("\n", household, "\n", "Participants:", "\n"participant1, "\n"participanname2, "\n"participanname3, "\n"participan4,)
+        print("1.", task1, amount_1)
+        print("2.", task2, amount_2)
+        print("3.", task3, amount_3)
+      continue
+#if log done execute
     elif chore_list == log_chores_done:
-        print("Section not complete please choose another option.")
-        continue
-#if leaderboard tell user incomplete
+      print("In progress please choose another option.")
+      continue
+#if leaderboard execute
     elif chore_list == show_leaderboard:
-        print("Section not complete please choose another option.")
-        continue
-#if quit exit program
+      print("In progress please choose another option.")
+      continue
+#if quit execute
     elif chore_list == quit:
-        print("Exiting application")
-        break
+      print("Exiting application")
+      break
 #if any other value tell user the error
     else:
-        print("Incorrect Input. Please select an option fro mthe menu")
-        continue
-
+      print("Incorrect Input.")
+      continue
 
 # Start the program
 if __name__ == "__main__":
